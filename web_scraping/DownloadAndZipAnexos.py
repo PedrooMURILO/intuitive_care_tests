2b89348d4bb5d6b6e0c22ddc430cf54c2d612b0d
Pedro Murilo <pedromarmindo@gmail.com>
2025-03-29 20:48:30 -0300

    Adicionando requests

---
+++ web_scraping/DownloadAndZipAnexos.py
@@ -0,0 +1,7 @@
+import requests
+
+url = "https://www.gov.br/ans/pt-br/acesso-a-informacao/participacao-da-sociedade/atualizacao-do-rol-de-procedimentos"
+
+response = requests.get(url)
+
+print(response.content)
