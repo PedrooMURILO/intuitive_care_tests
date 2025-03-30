47d7814511ce657fa6f49bcf0c8fc573d708e30a
Pedro Murilo <pedromarmindo@gmail.com>
2025-03-29 21:18:37 -0300

    Adicionando downloads

--- web_scraping/DownloadAndZipAnexos.py
+++ web_scraping/DownloadAndZipAnexos.py
@@ -1,8 +1,11 @@
 import requests
 from bs4 import BeautifulSoup
 from urllib.parse import urljoin
+import os
 
 url = "https://www.gov.br/ans/pt-br/acesso-a-informacao/participacao-da-sociedade/atualizacao-do-rol-de-procedimentos"
+
+os.makedirs("downloads", exist_ok=True)
 
 response = requests.get(url)
 
@@ -13,6 +16,18 @@ if response.status_code == 200:
     
     pdf_links = [urljoin(url, link["href"]) for link in links if "anexo" in link["href"].lower() and link["href"].lower().endswith(".pdf")]
     
-    print(pdf_links)
+    downloaded_files = []
+    for link in pdf_links:
+        file_name = link.split("/")[-1]
+        file_path = os.path.join("downloads", file_name)
+        pdf_response = requests.get(link, stream=True)
+        if pdf_response.status_code == 200:
+            with open(file_path, "wb") as file:
+                for chunk in pdf_response.iter_content(1024):
+                    file.write(chunk)
+            print(f"Download concluído: {file_path}")
+            downloaded_files.append(file_path)
+        else:
+            print(f"Erro ao baixar: {link}")
 else:
     print("Erro ao acessar o site.")
