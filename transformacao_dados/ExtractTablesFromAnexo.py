76e798223837b2b7607e6d2512a525ab38da99ef
Pedro Murilo <pedromarmindo@gmail.com>
2025-03-30 12:05:04 -0300

    Compactando o CSV

--- transformacao_dados/ExtractTablesFromAnexo.py
+++ transformacao_dados/ExtractTablesFromAnexo.py
@@ -1,5 +1,7 @@
 import tabula
 import pandas as pd
+import zipfile
+import os
 
 pdf_file = "Anexo_I_Rol_2021RN_465.2021_RN627L.2024.pdf"
 
@@ -20,5 +22,10 @@ for table in tables:
 final_table = pd.concat(processed_tables, ignore_index=True)
 
 csv_file = "dados_extraidos.csv"
-
 final_table.to_csv(csv_file, index=False, sep=';')
+
+zip_file = "Teste_Pedro_Armindo.zip"
+with zipfile.ZipFile(zip_file, 'w') as zipf:
+    zipf.write(csv_file, os.path.basename(csv_file))
+
+print(f"Arquivo CSV salvo e compactado em {zip_file}")
