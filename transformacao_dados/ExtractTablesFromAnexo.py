66ef85756eea6877f43eebec41136ed5e13845dc
Pedro Murilo <pedromarmindo@gmail.com>
2025-03-30 11:59:18 -0300

    Salvando em CSV

--- transformacao_dados/ExtractTablesFromAnexo.py
+++ transformacao_dados/ExtractTablesFromAnexo.py
@@ -1,7 +1,12 @@
 import tabula
+import pandas as pd
 
 pdf_file = "Anexo_I_Rol_2021RN_465.2021_RN627L.2024.pdf"
 
 tables = tabula.read_pdf(pdf_file, pages="3-181", multiple_tables=True, lattice=True)
 
-print(tables[0])
+final_table = pd.concat(tables, ignore_index=True)
+
+csv_file = "dados_extraidos.csv"
+
+final_table.to_csv(csv_file, index=False, sep=';')
