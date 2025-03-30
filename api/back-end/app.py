79eb2fa0209f941b949c0ad5922a07b23f04b38f
Pedro Murilo <pedromarmindo@gmail.com>
2025-03-30 13:40:51 -0300

    Implementação da função busca

--- api/back-end/app.py
+++ api/back-end/app.py
@@ -8,11 +8,24 @@ operadoras = []
 try:
     with open('Relatorio_cadop.csv', mode='r', encoding='utf-8') as file:
         operadoras = list(csv.DictReader(file, delimiter=';'))
-        print(operadoras[0])
 except FileNotFoundError:
     print("Erro: Arquivo 'Relatorio_cadop.csv' não encontrado.")
 except Exception as e:
     print(f"Erro ao ler o arquivo CSV: {e}")
+    
+    
+def buscar_operadoras(query):
+    resultados = []
+    
+    query = query.lower()
+    
+    for op in operadoras:
+        if (query in op['Razao_Social'].lower()) or (query in op['Nome_Fantasia'].lower()):
+            resultados.append(op)
+    
+    return resultados
+
+print(buscar_operadoras("administradora")[0])
 
 if __name__ == '__main__':
     app.run(debug=True)
