c5cac67156c41ec001ec4c79c230a96737b37354
Pedro Murilo <pedromarmindo@gmail.com>
2025-03-30 13:37:14 -0300

    Carregamento inicial do arquivo CSV

--- api/back-end/app.py
+++ api/back-end/app.py
@@ -1,6 +1,18 @@
 from flask import Flask, request, jsonify
+import csv
 
 app = Flask(__name__)
+
+operadoras = []
+
+try:
+    with open('Relatorio_cadop.csv', mode='r', encoding='utf-8') as file:
+        operadoras = list(csv.DictReader(file, delimiter=';'))
+        print(operadoras[0])
+except FileNotFoundError:
+    print("Erro: Arquivo 'Relatorio_cadop.csv' não encontrado.")
+except Exception as e:
+    print(f"Erro ao ler o arquivo CSV: {e}")
 
 if __name__ == '__main__':
     app.run(debug=True)
