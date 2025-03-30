64c6231a3bc3046cf8154f659b2c0b22af7c12d1
Pedro Murilo <pedromarmindo@gmail.com>
2025-03-30 12:03:02 -0300

    Estruturando o CSV corretamente

--- transformacao_dados/ExtractTablesFromAnexo.py
+++ transformacao_dados/ExtractTablesFromAnexo.py
@@ -3,9 +3,21 @@ import pandas as pd
 
 pdf_file = "Anexo_I_Rol_2021RN_465.2021_RN627L.2024.pdf"
 
+def replace_abbreviations(df):
+    df.replace({"OD": "Seg. Odontológica", "AMB": "Seg. Ambulatoria"}, inplace=True)
+    return df
+
 tables = tabula.read_pdf(pdf_file, pages="3-181", multiple_tables=True, lattice=True)
 
-final_table = pd.concat(tables, ignore_index=True)
+processed_tables = []
+for table in tables:
+    # Substituir abreviações
+    table = replace_abbreviations(table)
+    table.columns = table.columns.str.replace("\r", " ", regex=False)
+    table = table.map(lambda x: str(x).replace("\r", " "))
+    processed_tables.append(table)
+
+final_table = pd.concat(processed_tables, ignore_index=True)
 
 csv_file = "dados_extraidos.csv"
 
