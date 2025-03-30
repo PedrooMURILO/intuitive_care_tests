f9d3ad92ed12e29cbce24b731ccb9e79e437cf59
Pedro Murilo <pedromarmindo@gmail.com>
2025-03-29 21:12:27 -0300

    Adicionando BeautifulSoup

--- web_scraping/DownloadAndZipAnexos.py
+++ web_scraping/DownloadAndZipAnexos.py
@@ -1,7 +1,15 @@
 import requests
+from bs4 import BeautifulSoup
 
 url = "https://www.gov.br/ans/pt-br/acesso-a-informacao/participacao-da-sociedade/atualizacao-do-rol-de-procedimentos"
 
 response = requests.get(url)
 
-print(response.content)
+if response.status_code == 200:
+    soup = BeautifulSoup(response.text, "html.parser")
+    
+    links = soup.find_all("a", href=True)
+    
+    print(links)
+else:
+    print("Erro ao acessar o site.")
